c617ab24d29c38c7f8aee977eaa200e1db7f36d2
Veramelli <94964842+Veramelli@users.noreply.github.com>
2022-04-24 19:43:42 +0200

    Add files via upload

--- spam.js
+++ spam.js
@@ -4,19 +4,15 @@ function addMessage() {
     let text = document.querySelector('input').value;
     messages.push(text);
     generationMessages();
-    checkSpam();
+
 }
 
 function generationMessages() {
     let optionsString = '';
     for (let m of messages) {
-        optionsString += `<div>${m}</div>`
+        optionsString += `<div>${checkSpam(m)}</div>`
     }
     document.querySelector('.message').innerHTML = optionsString;
 }
 
-function checkSpam() {
-    let lowerStr = messages.toLowerCase();
-
-
-}
+const checkSpam = m => m.replace(/viagra|xxx/gi, "***");
